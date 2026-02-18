e3cd5303c271bf03ab54e11e069c005c9fd2596d
dge-ivan <ivannhdz03@gmail.com>
2026-02-18 17:05:21 -0600

    Cambio de dependencias en el enrutador

--- dev/Frontend/src/router/AppRouter.jsx
+++ dev/Frontend/src/router/AppRouter.jsx
@@ -1,12 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 //contador
-import Counter from '../pages/Counter/Counter';
+import Counter from '../features/Counter/Counter';
 //mainpages
-import HomePage from '../pages/home/Home';
-import LoginForm from '../pages/auth/LoginForm';
-import RegisterForm from '../pages/auth/RegisterForm';
-import { ForgotPasswd } from '../pages/auth/ForgotPass';
+import HomePage from '../features/home/Home';
+import LoginForm from '../features/auth/LoginForm';
+import RegisterPage from '../features/auth/register/ui/RegisterPage';
+import { ForgotPasswd } from '../features/auth/ForgotPass';
 
 const AppRouter = () => {
     return(
@@ -15,7 +15,7 @@ const AppRouter = () => {
                 <Route path="/example" element={<Counter />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="/iniciar-sesion" element={<LoginForm />} />
-                <Route path="/registrarse" element={<RegisterForm />} />
+                <Route path="/registrarse" element={<RegisterPage />} />
                 <Route path="/recuperar" element={<ForgotPasswd />} />
             </Routes>
         </BrowserRouter>
